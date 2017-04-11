8813f74289be81a3f787f0dc71b2d8cbf97ea948
Foxick <ub.verdg@gmail.com>
2017-04-11 20:01:53 +0300

    Init commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Programs/OpenServer/domains/OpenCart/catalog/');
+define('DIR_SYSTEM', 'D:/Programs/OpenServer/domains/OpenCart/system/');
+define('DIR_IMAGE', 'D:/Programs/OpenServer/domains/OpenCart/image/');
+define('DIR_LANGUAGE', 'D:/Programs/OpenServer/domains/OpenCart/catalog/language/');
+define('DIR_TEMPLATE', 'D:/Programs/OpenServer/domains/OpenCart/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/Programs/OpenServer/domains/OpenCart/system/config/');
+define('DIR_CACHE', 'D:/Programs/OpenServer/domains/OpenCart/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Programs/OpenServer/domains/OpenCart/system/storage/download/');
+define('DIR_LOGS', 'D:/Programs/OpenServer/domains/OpenCart/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Programs/OpenServer/domains/OpenCart/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Programs/OpenServer/domains/OpenCart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'nastja');
+define('DB_PASSWORD', '10099525');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
